87b861c94b5c28e1e43607181a039fbb4f76ef94
gnagnaonni <104013405+gnagnaonni@users.noreply.github.com>
2025-02-19 12:59:09 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -296,7 +296,7 @@ function doSubmit() {
 
   setBtnLoading()
 
-  let scriptUrl = "https://script.google.com/macros/s/AKfycbzKIQE4MgO8LzwcnAXpCh9SPZd2m2wTBhCE8q9cg7fSly-t312rjw38PP9y5MNv_PcGDA/exec";
+  let scriptUrl = "https://script.google.com/macros/s/AKfycbwJY5pUSO_3hjVDk9OmUj2PtXdNIUjCBSHXclxXHGZBWlRaNujTOJD-_WGPWn3tHObK/exec";
   let formData = {
     name: document.getElementById('form_name').value,
     email: document.getElementById('form_email').value,
